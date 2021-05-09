48fc8f320bf25a3d9f900a6494a239e36d7c6d41
s0t029i <supriya.tiwari@walmart.com>
2021-05-09 22:18:45 +0530

    commit1

--- FNP_UI_AUTOMATION/src/test/java/com/test/fnp/TestClass1.java
+++ FNP_UI_AUTOMATION/src/test/java/com/test/fnp/TestClass1.java
@@ -19,9 +19,9 @@ public class TestClass1 extends BaseClass{
 	 homePage=new HomePage(driver);
 	}
 	
-	// To verify Home Page -- 
-	//test
-	@Test
+	// To verify Home Page 
+	//Test1
+	@Test 
 	public void verifyMainPage() {
 		String page= EnvironmentConfig.getInstance().getProperty("pageurl");
 		System.out.println(page);
